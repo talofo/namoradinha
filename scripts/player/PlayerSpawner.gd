4c19fe0c9f89ed53919e8b3c6bfde5ad6f846dd3
oikram <talofo@gmail.com>
2025-03-28 08:52:23 +0000

    WIP: Start testing the bounce with zoom out camera

--- scripts/player/PlayerSpawner.gd
+++ scripts/player/PlayerSpawner.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @export var player_scene: PackedScene
-@export var spawn_position: Vector2 = Vector2(-900, -75)
+@export var spawn_position: Vector2 = Vector2(-7900, -75)
 
 var player_instance: Node = null
 
@@ -19,4 +19,4 @@ func spawn_player():
 
 	# Immediately launch it downward with an initial velocity
 	if player_instance.has_method("launch"):
-		player_instance.launch(Vector2(300, -1000))  # Adjust power as needed
+		player_instance.launch(Vector2(1400, -2000))  # 300, -1000 but, adjust power as needed
