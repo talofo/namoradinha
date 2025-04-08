5ba0fa5ca2271841d985586afa2ee21f9e950026
oikram <talofo@gmail.com>
2025-04-08 18:39:35 +0100

    clear warnings still persistent after system refactoring

--- scripts/motion/subsystems/bounce_system/data/CollisionContext.gd
+++ scripts/motion/subsystems/bounce_system/data/CollisionContext.gd
@@ -2,7 +2,7 @@ class_name CollisionContext
 extends RefCounted
 
 # Preload dependent data structures
-const IncomingMotionState = preload("res://scripts/motion/subsystems/bounce_system/data/IncomingMotionState.gd")
+const IncomingMotionStateClass = preload("res://scripts/motion/subsystems/bounce_system/data/IncomingMotionState.gd")
 const ImpactSurfaceDataClass = preload("res://scripts/motion/subsystems/bounce_system/data/ImpactSurfaceData.gd")
 const PlayerBounceProfileClass = preload("res://scripts/motion/subsystems/bounce_system/data/PlayerBounceProfile.gd")
 
@@ -31,7 +31,7 @@ func _init(
 	p_current_gravity: Vector2 = Vector2.DOWN * ProjectSettings.get_setting("physics/2d/default_gravity", 980.0),
 	p_generate_debug_data: bool = false
 ) -> void:
-	incoming_motion_state = p_incoming_motion_state if p_incoming_motion_state != null else IncomingMotionState.new()
+	incoming_motion_state = p_incoming_motion_state if p_incoming_motion_state != null else IncomingMotionStateClass.new()
 	impact_surface_data = p_impact_surface_data if p_impact_surface_data != null else ImpactSurfaceDataClass.new()
 	player_bounce_profile = p_player_bounce_profile if p_player_bounce_profile != null else PlayerBounceProfileClass.new()
 	current_gravity = p_current_gravity
